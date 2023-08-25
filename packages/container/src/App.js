196435f46c49f4c59fecb70406f991e864665317
Manan Chawla <mananchawla@Manans-Air.Dlink>
2023-08-26 00:13:15 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/marketingApp";
+// import { mount } from "marketing/MarketingApp";
+
+// console.log(mount);
+export default () => {
+  return (
+    <>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
